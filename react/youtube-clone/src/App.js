b1d77603a965820e03d908fb1708a6f0286509db
awantech-io <hello.wanlab@gmail.com>
2020-10-16 08:35:20 +0800

    first commit

---
+++ react/youtube-clone/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      yt
+    </div>
+  );
+}
+
+export default App;
---
+++ react/youtube-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDj7yqcSEd6hRrrw96okuPyl9DXECYvoMU",
+    authDomain: "clone-211c7.firebaseapp.com",
+    databaseURL: "https://clone-211c7.firebaseio.com",
+    projectId: "clone-211c7",
+    storageBucket: "clone-211c7.appspot.com",
+    messagingSenderId: "365233150643",
+    appId: "1:365233150643:web:a9384fabe9c272b65f6ec6",
+    measurementId: "G-QYBEXE0Q07"
+  };
